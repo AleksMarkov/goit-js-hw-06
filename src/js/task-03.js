9af81ddb1d9181c2e51ef36ff67e58ea1d8080e1
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-10-21 23:20:34 -0400

    task-03
    resolved

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,14 +13,15 @@ const images = [
   },
 ];
 
-// elem.parentNode - вибере батьківський elem.
-// elem.childNodes - псевдомасив, зберігає всі дочірні елементи, включно з текстовими.
-// elem.children - псевдомасив, зберігає тільки дочірні вузли-елементи, тобто ті, що відповідають тегам.
-// elem.firstChild - вибере перший дочірній елемент всередині elem, включно з текстовими вузлами.
-// elem.firstElementChild - вибере перший дочірній вузол-елемент всередині elem.
-// elem.lastChild - вибере останній дочірній елемент всередині elem, включно з текстовими вузлами.
-// elem.lastElementChild - вибере останній дочірній вузол-елемент всередині elem.
-// elem.previousSibling - вибере елемент «зліва» від elem (його попереднього сусіда).
-// elem.previousElementSibling - вибере вузол-елемент «зліва» від elem (його попереднього сусіда).
-// elem.nextSibling - вибере елемент «праворуч» від elem (його наступного сусіда)
-// elem.nextElementSibling - вибере вузол-елемент «праворуч» від elem (його наступного сусіда).
+const list = document.querySelector(".gallery");
+let imgEl = ``;
+
+images.forEach(myFunction);
+
+function myFunction(item) {
+  imgEl =
+    imgEl +
+    `<li class="gallery-item"><img src="${item.url}" alt="${item.alt}"></img></li>`;
+}
+
+list.insertAdjacentHTML("afterbegin", imgEl);
