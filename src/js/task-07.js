c2517c2f7d304c6600c8d056dddd0b4c72d51829
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-10-22 18:47:17 -0400

    task-08
    solved

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,5 +1,8 @@
-const input = document.getElementById("font-size-control");
-const span = document.getElementById("text");
+// Напиши скрипт, який реагує на зміну значення input#font - size - control(подія input)
+// і змінює інлайн - стиль span#text, оновлюючи властивість font - size.В результаті,
+//     перетягуючи повзунок, буде змінюватися розмір тексту.
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
 
 input.addEventListener("change", handleInput);
 
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleInput);
+
+function handleInput(event) {
+  event.preventDefault();
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("Всі поля повинні бути заповнені");
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+}
