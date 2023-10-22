2693ec1531c8695189438a94f6254f6f74cb2d69
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-10-22 19:30:05 -0400

    task-10
    solved

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxesDiv = document.getElementById("boxes");
+const input = document.querySelector("input");
+
+destroyButton.addEventListener("click", handleDestroyClick);
+createButton.addEventListener("click", handleCreateClick);
+
+function handleCreateClick() {
+  const numberBox = Number(document.querySelector("input").value);
+  input.disabled = true;
+  createBoxes(numberBox);
+}
+
+function handleDestroyClick() {
+  boxesDiv.innerHTML = "";
+  input.disabled = false;
+  input.value = "";
+}
+
+function createBoxes(boxes) {
+  for (let i = 0; i < boxes; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    boxesDiv.appendChild(box);
+  }
+}
